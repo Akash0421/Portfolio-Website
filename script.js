35991389e16b5783c7b7c64a91ef844b2988b3fb
Akash0421 <105042798+Akash0421@users.noreply.github.com>
2024-04-09 14:59:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,3 @@ let typed = new Typed(".multiple-text", {
     backDelay:1000,
     loop:true
 })
-
-function onStartedDownload(id) {
-    console.log(`Started downloading: ${id}`);
-  }
-  
-  function onFailed(error) {
-    console.log(`Download failed: ${error}`);
-  }
-  
-  let downloadUrl = "https://example.org/image.png";
-  
-  let downloading = browser.downloads.download({
-    url: downloadUrl,
-    filename: "my-image-again.png",
-    conflictAction: "uniquify",
-  });
-  
-  downloading.then(onStartedDownload, onFailed);
